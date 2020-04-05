319558908f3ca96e7c4920fa23ed7a6b652a34cb
MelanX <meissi97@me.com>
2020-04-05 17:15:45 +0200

    move config to defaultconfigs path
    close #8

--- src/main/java/de/melanx/morevanillalib/MoreVanillaLib.java
+++ src/main/java/de/melanx/morevanillalib/MoreVanillaLib.java
@@ -9,6 +9,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLConfig;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,7 +26,7 @@ public class MoreVanillaLib {
     public MoreVanillaLib() {
         instance = this;
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, LibConfigHandler.SERVER_CONFIG);
-        LibConfigHandler.loadConfig(LibConfigHandler.SERVER_CONFIG, FMLPaths.CONFIGDIR.get().resolve(MODID + "-server.toml"));
+        LibConfigHandler.loadConfig(LibConfigHandler.SERVER_CONFIG, FMLPaths.GAMEDIR.get().resolve(FMLConfig.defaultConfigPath()).resolve(MODID + "-server.toml"));
         MinecraftForge.EVENT_BUS.register(this);
     }
 
