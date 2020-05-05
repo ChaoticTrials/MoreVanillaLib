16305f1ac565c34f897fc50905108f73bb01bae1
MelanX <git@melanx.de>
2020-05-05 12:58:55 +0200

    use localized values for repairing item
    close #12

--- src/main/java/de/melanx/morevanillalib/compat/JeiCompat.java
+++ src/main/java/de/melanx/morevanillalib/compat/JeiCompat.java
@@ -55,20 +55,76 @@ public class JeiCompat implements IModPlugin {
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        addValueInfoPage(registration, ModTags.Items.BONE_TOOLS.getAllElements(), "bone_tools", LibConfigHandler.boneDurability.get(), LibConfigHandler.boneDurability.get() * 5, LibConfigHandler.boneHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.BONE.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.COAL_TOOLS.getAllElements(), "coal_tools", LibConfigHandler.coalDurability.get(), LibConfigHandler.coalDurability.get() * 5, LibConfigHandler.coalHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.COAL.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.EMERALD_TOOLS.getAllElements(), "emerald_tools", LibConfigHandler.emeraldDurability.get(), LibConfigHandler.emeraldDurability.get() * 5, LibConfigHandler.emeraldHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.EMERALD.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.ENDER_TOOLS.getAllElements(), "ender_tools", LibConfigHandler.enderDurability.get(), LibConfigHandler.enderDurability.get() * 5, LibConfigHandler.enderHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.ENDER.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.FIERY_TOOLS.getAllElements(), "fiery_tools", LibConfigHandler.fieryDurability.get(), LibConfigHandler.fieryDurability.get() * 5, LibConfigHandler.fieryHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.FIERY.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.GLOWSTONE_TOOLS.getAllElements(), "glowstone_tools", LibConfigHandler.glowstoneDurability.get(), LibConfigHandler.glowstoneDurability.get() * 5, LibConfigHandler.glowstoneHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.GLOWSTONE.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.LAPIS_TOOLS.getAllElements(), "lapis_tools", LibConfigHandler.lapisDurability.get(), LibConfigHandler.lapisDurability.get() * 5, LibConfigHandler.lapisHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.LAPIS.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.NETHER_TOOLS.getAllElements(), "nether_tools", LibConfigHandler.netherDurability.get(), LibConfigHandler.netherDurability.get() * 5, LibConfigHandler.netherHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.NETHER.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.OBSIDIAN_TOOLS.getAllElements(), "obsidian_tools", LibConfigHandler.obsidianDurability.get(), LibConfigHandler.obsidianDurability.get() * 5, LibConfigHandler.obsidianHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.OBSIDIAN.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.PAPER_TOOLS.getAllElements(), "paper_tools", LibConfigHandler.paperDurability.get(), LibConfigHandler.paperDurability.get() * 5, LibConfigHandler.paperHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.PAPER.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.PRISMARINE_TOOLS.getAllElements(), "prismarine_tools", LibConfigHandler.prismarineDurability.get(), LibConfigHandler.prismarineDurability.get() * 5, LibConfigHandler.prismarineHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.PRISMARINE.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.QUARTZ_TOOLS.getAllElements(), "quartz_tools", LibConfigHandler.quartzDurability.get(), LibConfigHandler.quartzDurability.get() * 5, LibConfigHandler.quartzHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.QUARTZ.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.REDSTONE_TOOLS.getAllElements(), "redstone_tools", LibConfigHandler.redstoneDurability.get(), LibConfigHandler.redstoneDurability.get() * 5, LibConfigHandler.redstoneHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.REDSTONE.getRepairMaterial()));
-        addValueInfoPage(registration, ModTags.Items.SLIME_TOOLS.getAllElements(), "slime_tools", LibConfigHandler.slimeDurability.get(), LibConfigHandler.slimeDurability.get() * 5, LibConfigHandler.slimeHarvestlevel.get(), getItemFromIngredient(BigBreakMaterials.SLIME.getRepairMaterial()));
+        addValueInfoPage(registration, ModTags.Items.BONE_TOOLS.getAllElements(), "bone_tools",
+                LibConfigHandler.boneDurability.get(),
+                LibConfigHandler.boneDurability.get() * 5,
+                LibConfigHandler.boneHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.BONE.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.COAL_TOOLS.getAllElements(), "coal_tools",
+                LibConfigHandler.coalDurability.get(),
+                LibConfigHandler.coalDurability.get() * 5,
+                LibConfigHandler.coalHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.COAL.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.EMERALD_TOOLS.getAllElements(), "emerald_tools",
+                LibConfigHandler.emeraldDurability.get(),
+                LibConfigHandler.emeraldDurability.get() * 5,
+                LibConfigHandler.emeraldHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.EMERALD.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.ENDER_TOOLS.getAllElements(), "ender_tools",
+                LibConfigHandler.enderDurability.get(),
+                LibConfigHandler.enderDurability.get() * 5,
+                LibConfigHandler.enderHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.ENDER.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.FIERY_TOOLS.getAllElements(), "fiery_tools",
+                LibConfigHandler.fieryDurability.get(),
+                LibConfigHandler.fieryDurability.get() * 5,
+                LibConfigHandler.fieryHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.FIERY.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.GLOWSTONE_TOOLS.getAllElements(), "glowstone_tools",
+                LibConfigHandler.glowstoneDurability.get(),
+                LibConfigHandler.glowstoneDurability.get() * 5,
+                LibConfigHandler.glowstoneHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.GLOWSTONE.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.LAPIS_TOOLS.getAllElements(), "lapis_tools",
+                LibConfigHandler.lapisDurability.get(),
+                LibConfigHandler.lapisDurability.get() * 5,
+                LibConfigHandler.lapisHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.LAPIS.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.NETHER_TOOLS.getAllElements(), "nether_tools",
+                LibConfigHandler.netherDurability.get(),
+                LibConfigHandler.netherDurability.get() * 5,
+                LibConfigHandler.netherHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.NETHER.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.OBSIDIAN_TOOLS.getAllElements(), "obsidian_tools",
+                LibConfigHandler.obsidianDurability.get(),
+                LibConfigHandler.obsidianDurability.get() * 5,
+                LibConfigHandler.obsidianHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.OBSIDIAN.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.PAPER_TOOLS.getAllElements(), "paper_tools",
+                LibConfigHandler.paperDurability.get(),
+                LibConfigHandler.paperDurability.get() * 5,
+                LibConfigHandler.paperHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.PAPER.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.PRISMARINE_TOOLS.getAllElements(), "prismarine_tools",
+                LibConfigHandler.prismarineDurability.get(),
+                LibConfigHandler.prismarineDurability.get() * 5,
+                LibConfigHandler.prismarineHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.PRISMARINE.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.QUARTZ_TOOLS.getAllElements(), "quartz_tools",
+                LibConfigHandler.quartzDurability.get(),
+                LibConfigHandler.quartzDurability.get() * 5,
+                LibConfigHandler.quartzHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.QUARTZ.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.REDSTONE_TOOLS.getAllElements(), "redstone_tools",
+                LibConfigHandler.redstoneDurability.get(),
+                LibConfigHandler.redstoneDurability.get() * 5,
+                LibConfigHandler.redstoneHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.REDSTONE.getRepairMaterial()).getName().getFormattedText());
+        addValueInfoPage(registration, ModTags.Items.SLIME_TOOLS.getAllElements(), "slime_tools",
+                LibConfigHandler.slimeDurability.get(),
+                LibConfigHandler.slimeDurability.get() * 5,
+                LibConfigHandler.slimeHarvestlevel.get(),
+                getItemFromIngredient(BigBreakMaterials.SLIME.getRepairMaterial()).getName().getFormattedText());
 
         if (LibConfigHandler.extraDrop.get())
             addValueInfoPage(registration, ModTags.Items.ALL_TOOLS.getAllElements(), "extra_drop", (double) LibConfigHandler.extraDropChance.get() / 10);
@@ -83,11 +139,15 @@ public class JeiCompat implements IModPlugin {
         }
 
         if (LibConfigHandler.headDrop.get()) {
-            addValueInfoPage(registration, ModTags.Items.BONE_TOOLS.getAllElements(), "bone_heads", (double) LibConfigHandler.headDropChance.get() / 10);
+            addValueInfoPage(registration, ModTags.Items.BONE_TOOLS.getAllElements(), "bone_heads",
+                    (double) LibConfigHandler.headDropChance.get() / 10);
         }
 
         if (LibConfigHandler.damageByPaperTools.get()) {
-            addValueInfoPage(registration, ModTags.Items.PAPER_TOOLS.getAllElements(), "paper_damage", (double) LibConfigHandler.damageByPaperToolsChance.get() / 10, (double) LibConfigHandler.minPaperDamage.get() / 2, (double) LibConfigHandler.maxPaperDamage.get() / 2);
+            addValueInfoPage(registration, ModTags.Items.PAPER_TOOLS.getAllElements(), "paper_damage",
+                    (double) LibConfigHandler.damageByPaperToolsChance.get() / 10,
+                    (double) LibConfigHandler.minPaperDamage.get() / 2,
+                    (double) LibConfigHandler.maxPaperDamage.get() / 2);
         }
 
         if (LibConfigHandler.autoSmelt.get()) {
